7e9dfbb8996fcc087fc4cc9d83af266035434207
Jhonatan Estiven Sepúlveda Metrio <jsepulveda@CIDENET.LOCAL>
2020-01-03 15:04:23 -0500

    Spring Initializer (mvn) -> Spring Boot DevTools, Spring Web, Spring Data JPA, H2 Database

---
+++ src/test/java/com/jssm/hulkstore/HulkStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jssm.hulkstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulkStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
